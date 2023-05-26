1dfd52dd8ceea8cce7ffe9d87a06d50c2f943fe1
lcw3176 <lcw3176@naver.com>
2023-05-26 18:46:37 +0900

    feat: 더미데이터 테스트

--- src/store/job.js
+++ src/store/job.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia"
 export const useJobStore = defineStore("jobStore", {
 
   state: () => ({
-
+    jobs: [],
   }),
 
 
@@ -12,6 +12,36 @@ export const useJobStore = defineStore("jobStore", {
   },
 
   actions: {
+    async loadJobSchedules(){
+      // axios 드갈 부분
+      // 일단 더미 데이터로
+      this.jobs.length = 0;
+      let now = new Date();
 
-  }
+      let data = {
+        start: this.addDays(now, 1),
+        end: this.addDays(now, 7),
+        name: "맵샷컴퍼니",
+        link: "",
+      }
+
+      this.jobs.push(data);
+
+      data = {
+        start: this.addDays(now, 2),
+        end:  this.addDays(now, 5),
+        name: "방가방가IT",
+        link: "",
+      }
+
+      this.jobs.push(data);
+    },
+
+    addDays(date, days) {
+      const clone = new Date(date);
+      clone.setDate(date.getDate() + days)
+      
+      return clone.getDate();
+    }
+  },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,6 @@
 
       <v-app-bar elevation="0"></v-app-bar>
 
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-navigation-drawer location="right">
-        <v-list>
-          <v-list-item></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      
       <v-main>
         <v-card color="blue-grey-darken-1">
           <v-card-title class="text-center justify-center py-6">
@@ -32,7 +20,43 @@
             </span>
             <span>(탁해)</span>
           </v-card-title>
-          <Calendar />
+
+          <div>
+            <v-tabs align-tabs="center">
+              <v-tab @click="changeMonth(-1)">&lt; Before</v-tab>
+              <v-card-title>{{ calendarHeader }}</v-card-title>
+              <v-tab @click="changeMonth(1)">Next &gt;</v-tab>
+            </v-tabs>
+            <v-table>
+              <thead>
+                <tr>
+                  <th v-for="index in week" :key="index">
+                    {{ index }}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tr v-for="(index, i) in days" :key="i">
+
+                  <td v-for="childIndex in index" :key="childIndex">
+                    {{ childIndex }}
+
+                    <div v-if="childIndex != null" v-for="(item, i) in jobs">
+                      
+                      <v-radio-group v-if="item.start === childIndex || item.end === childIndex"
+                        v-model="item.toggle" color="primary">
+                        <v-radio :label="item.name" :value=item.name></v-radio>
+                      </v-radio-group>
+
+                    </div>
+                  </td>
+
+                </tr>
+              </tbody>
+            </v-table>
+          </div>
+
         </v-card>
       </v-main>
 
@@ -44,12 +68,82 @@
 
 <script>
 
-import Calendar from '@/components/Calendar.vue'
+import { useJobStore } from '@/store/job';
+import { storeToRefs } from 'pinia'
 
 export default {
   name: 'HomeView',
-  components: {
-    Calendar
-  }
+
+  setup() {
+    let jobStore = useJobStore();
+
+    jobStore.loadJobSchedules();
+
+    const { jobs } = storeToRefs(jobStore)
+    return {
+      jobs,
+    }
+  },
+
+
+  data() {
+    return {
+      today: new Date(),
+      week: ['일', '월', '화', '수', '목', '금', '토'],
+      calendarHeader: '',
+      days: [],
+      tab: null,
+    };
+  },
+  mounted() {
+    this.calendarImplementation();
+  },
+  methods: {
+    calendarImplementation: function () {
+
+      this.days = [];
+      const year = this.today.getFullYear();
+      const month = this.today.getMonth();
+
+      const startDayOfTheMonth = new Date(year, month, 1).getDay();
+
+      const endDayOfTheMonth = new Date(year, month + 1, 0).getDate();
+
+      const basicDays = Array.from({ length: endDayOfTheMonth }, (v, i) => i + 1);
+
+      const emptyDays = Array(startDayOfTheMonth).fill(null);
+
+      const combinedDays = [...emptyDays, ...basicDays];
+
+      for (let i = 0; i < endDayOfTheMonth + startDayOfTheMonth; i += 7) {
+        this.days.push(combinedDays.slice(i, i + 7));
+      }
+      this.calendarHeader = `${year}년 ${month + 1} 월`;
+      this.addLastWeekEmptyDays();
+    },
+
+    changeMonth: function (val) {
+      this.today = new Date(this.today.setMonth(this.today.getMonth() + val, 1));
+      this.calendarImplementation();
+    },
+
+    addLastWeekEmptyDays: function () {
+      const daysLastIndex = this.days.length - 1;
+      if (this.days[daysLastIndex] !== 7) this.days[daysLastIndex].length = 7;
+    },
+  },
 };
 </script>
+
+
+<style scoped>
+tr td:first-child,
+tr th:first-child {
+  color: red;
+}
+
+tr td:last-child,
+tr th:last-child {
+  color: blue;
+}
+</style>
